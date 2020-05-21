efbb415c3cb2d44003e692d708914ebfc61f815b
Puneet-Agarwal-01 <63462560+Puneet-Agarwal-01@users.noreply.github.com>
2020-05-21 14:11:50 +0530

    Create size.cpp

---
+++ size.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"size of char:"<<sizeof(char)<<"\n"
+        <<"size of int:"<<sizeof(int)<<"\n"
+        <<"size of float:"<<sizeof(float)<<"\n"
+        <<"size of double:"<<sizeof(double);
+    return 0;
+}     
